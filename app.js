23cb4586da07a49eed5ba04097b8b35ea6917dfb
colorlessenergy <brianmunozdev@gmail.com>
2020-06-17 17:39:03 -0700

    backend logic for saving a user with UID provided by firebase auth

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const config = require('./config/config');
+
+mongoose.connect(config.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('connected to DB'))
+  .catch(err => console.log('there was an error ' + err))
+
+const bodyParser = require('body-parser');
+
+// dump json in body into req.body
+app.use(bodyParser.json());
+
+
+const userRouter = require('./routes/userRouter');
+
+app.use('/users', userRouter);
 
 app.listen(process.env.PORT || 3001, function () {
   console.log('listening on port 3001');
