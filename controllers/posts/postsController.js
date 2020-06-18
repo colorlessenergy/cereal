e8e8278198b76bac9b78c0e35395d4e69a27fa26
colorlessenergy <brianmunozdev@gmail.com>
2020-06-18 12:30:42 -0700

    add comments

--- controllers/posts/postsController.js
+++ controllers/posts/postsController.js
@@ -7,9 +7,18 @@ exports.getAllPost = function (req, res, next) {
     .catch((err) => res.status(500).send('internal server error '));
 }
 
+/**
+  create a post. 
+  Save the User mongo generated ID to the post and save the ID of the post created to the user.
+
+  @param {String} req.body.FIREBASE_ID - ID provided by firebase auth and saved in the user
+  @param {String} req.body.cereal - type of cereal the post is about
+  @param {String} req.body.content - review of the cereal
+  @return {Object} - created post
+*/
+
 exports.createPost = function (req, res, next) {
   if (req.body['FIREBASE_ID']) {
-
     Users.findOne({ 'FIREBASE_ID': req.body.FIREBASE_ID })
       .then(function (user) {
         let formatData = {
@@ -17,6 +26,9 @@ exports.createPost = function (req, res, next) {
           content: req.body.content,
           createdBy: user._id
         };
+
+        // store the saved post in outer scope to be able
+        // to return it later in a different inner scope
         let savedPost = null;
 
         let post = new Posts(formatData);
