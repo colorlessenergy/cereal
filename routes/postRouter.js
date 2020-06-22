dbfe4835c9bc132fd7107981614cc28cf384e48c
colorlessenergy <brianmunozdev@gmail.com>
2020-06-22 08:41:40 -0700

    add a router, controller to retrieve a single post by id

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -3,9 +3,12 @@ const Router = express.Router();
 
 const postsController = require('../controllers/posts/postsController');
 
+
+Router.route('/:id')
+  .get(postsController.getSinglePostById)
+
 Router.route('/')
   .get(postsController.getAllPost)
   .post(postsController.createPost);
-
 
 module.exports = Router;
